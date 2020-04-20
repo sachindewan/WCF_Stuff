238a20b0f9ea6eee9a90d0d61fc3eda24389281c
Sachin Kumar <sachin.kumar06@nagarro.com>
2020-04-20 10:28:29 +0530

    Initail commit wcf_services 1. Services contract has been designed 2. operation contact has been designed 3.implementing the service contract in wcf service, thet will be exposed to the client.

---
+++ WCFService1/App_Code/IUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+namespace SampleWcfService
+{
+    [ServiceContract]
+    public interface IUser
+    {
+        [OperationContract]
+        string WelcomeUser(string userName);
+
+        [OperationContract]
+        bool AuthenticateUser(string userName, string password);
+    }
+}
---
+++ WCFService1/App_Code/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace SampleWcfService
+{
+    public class User : IUser
+    {
+        public bool AuthenticateUser(string userName, string password)
+        {
+            if(userName=="wcf" && password == "sachin@123")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string WelcomeUser(string userName)
+        {
+            return "wolcome " + userName;
+        }
+    }
+}
