a041ac4e8b667d7ba2209611cf9e08bef3795bf5
Sachin Kumar <sachin.kumar06@nagarro.com>
2020-04-20 14:04:58 +0530

    consuming duplex service

---
+++ Duplex_WCF/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Duplex_WCF.SampleWcfService;
+
+namespace Duplex_WCF
+{
+    [CallbackBehavior(UseSynchronizationContext =false)]
+    class Program:IProcessCallback
+    {
+        public void TaskProgress(int progress)
+        {
+            Console.WriteLine(progress + "% task has been completed");
+          
+        }
+
+        static void Main(string[] args)
+        {
+           
+            Program program = new Program();
+            program.processTask();
+            Console.ReadLine();
+        }
+        private void processTask()
+        {
+            InstanceContext instanceContext = new InstanceContext(this);
+            SampleWcfService.ProcessClient processClient = new SampleWcfService.ProcessClient(instanceContext);
+            processClient.TaskProcess();
+        }
+
+    }
+}
