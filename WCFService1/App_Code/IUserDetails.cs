27b0d5cb01999db83c943a90abfbaf2e17044896
Sachin Kumar <sachin.kumar06@nagarro.com>
2020-04-20 11:58:23 +0530

    implemented net.tcp bindings

---
+++ WCFService1/App_Code/IUserDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+namespace SampleWcfService
+{
+    [ServiceContract]
+    public interface IUserDetails
+    {
+        [OperationContract]
+        List<string> GetUserDetals();
+
+    }
+}
--- WCFService1/App_Code/User.cs
+++ WCFService1/App_Code/User.cs
@@ -7,7 +7,7 @@ using System.ServiceModel;
 
 namespace SampleWcfService
 {
-    public class User : IUser
+    public class User : IUser, IUserDetails
     {
         public bool AuthenticateUser(string userName, string password)
         {
@@ -19,6 +19,17 @@ namespace SampleWcfService
             {
                 return false;
             }
+        }
+
+        public List<string> GetUserDetals()
+        {
+            var users = new object[] { "Sachin", "riyan rajput", "gaurav singh", "monu", "monu band", "sandeep", "kapil", "sarvesh", "kuldeep" };
+            List<string> lstString = new List<string>();
+            foreach (var item in users)
+            {
+                lstString.Add(item.ToString());
+            }
+            return lstString;
         }
 
         public string WelcomeUser(string userName)
